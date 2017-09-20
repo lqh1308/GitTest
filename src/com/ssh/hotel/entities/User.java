0a50ed631a1e0559fc86840cdae0c85e0af4f865
lqh1308 <2247872547@qq.com>
2017-09-20 15:15:08 +0800

    测试

--- src/com/ssh/hotel/entities/User.java
+++ src/com/ssh/hotel/entities/User.java
@@ -4,6 +4,16 @@ public class User {
 	private String userName;
 	
 	private String password;
+	
+	private String sex;
+	
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	
+	public String getSex() {
+		return sex;
+	}
 
 	public String getUserName() {
 		return userName;
